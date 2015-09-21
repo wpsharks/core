eb24a8c9596c673e88025175369d105029315cd9
jaswsinc <jas@wsharks.com>
2015-09-21 15:28:09 -0800

    Type hinting `Cookie`.

--- src/includes/classes/Cookie.php
+++ src/includes/classes/Cookie.php
@@ -32,32 +32,55 @@ class Cookie extends AbsBase
      *
      * @param string $name          Name of the cookie.
      * @param string $value         Value for this cookie (empty to delete).
-     * @param string $key           Optional. Key to use in cookie encryption.
      * @param int    $expires_after Optional. Time (in seconds) this cookie should last for.
      *                              Defaults to `31556926` (one year). If this is set to anything <= `0`,
      *                              the cookie will expire automatically after the current browser session.
+     * @param string $domain        Optional input domain name to set the cookie for.
+     * @param string $key           Optional. Key to use in cookie encryption.
      *
      * @throws \Exception If headers have already been sent; i.e. if not possible.
      */
-    public function set($name, $value, $key = '', $expires_after = 31556926)
+    public function set(string $name, string $value, int $expires_after = 31556926, string $domain = '', string $key = '')
     {
         if (headers_sent()) {
             throw new \Exception('Doing it wrong! Headers sent already.');
         }
-        if (!($name = trim((string) $name))) {
+        if (!($name = trim($name))) {
             return; // Not possible.
         }
-        $value         = (string) $value;
-        $expires_after = max(0, (integer) $expires_after);
-        $key           = (string) $key; // Encryption key.
+        $expires_after = max(0, $expires_after);
+        $value         = $value ? $this->Rijndael256->encrypt($value, $key) : '';
+        $expires       = $expires_after ? time() + $expires_after : 0;
 
-        $value   = $value ? $this->Rijndael256->encrypt($value, $key) : '';
-        $expires = $expires_after ? time() + $expires_after : 0;
-
-        $cookie_path      = defined('COOKIEPATH') ? (string) COOKIEPATH : '/';
-        $site_cookie_path = defined('SITECOOKIEPATH') ? (string) SITECOOKIEPATH : '/';
-        $cookie_domain    = defined('COOKIE_DOMAIN') ? (string) COOKIE_DOMAIN : $this->UrlCurrent->host(true);
-
+        if (defined('COOKIE_PATH')) {
+            $cookie_path = (string) COOKIE_PATH;
+        } elseif (defined('COOKIEPATH')) {
+            $cookie_path = (string) COOKIEPATH;
+        } elseif (!empty($_SERVER['COOKIE_PATH'])) {
+            $cookie_path = (string) $_SERVER['COOKIE_PATH'];
+        } else {
+            $cookie_path = '/';
+        }
+        if (defined('SITE_COOKIE_PATH')) {
+            $site_cookie_path = (string) SITE_COOKIE_PATH;
+        } elseif (defined('SITECOOKIEPATH')) {
+            $site_cookie_path = (string) SITECOOKIEPATH;
+        } elseif (!empty($_SERVER['SITE_COOKIE_PATH'])) {
+            $site_cookie_path = (string) $_SERVER['SITE_COOKIE_PATH'];
+        } else {
+            $site_cookie_path = '/';
+        }
+        if ($domain) {
+            $cookie_domain = $domain;
+        } elseif (defined('COOKIE_DOMAIN')) {
+            $cookie_domain = (string) COOKIE_DOMAIN;
+        } elseif (defined('COOKIEDOMAIN')) {
+            $cookie_domain = (string) COOKIEDOMAIN;
+        } elseif (!empty($_SERVER['COOKIE_DOMAIN'])) {
+            $cookie_domain = (string) $_SERVER['COOKIE_DOMAIN'];
+        } else {
+            $cookie_domain = $this->UrlCurrent->host(true);
+        }
         setcookie($name, $value, $expires, $cookie_path, $cookie_domain);
         setcookie($name, $value, $expires, $site_cookie_path, $cookie_domain);
 
@@ -74,13 +97,11 @@ class Cookie extends AbsBase
      *
      * @return string Cookie string value (unencrypted).
      */
-    public function get($name, $key = '')
+    public function get(string $name, string $key = ''): string
     {
-        if (!($name = (string) $name)) {
+        if (!($name = trim($name))) {
             return ''; // Not possible.
         }
-        $key = (string) $key; // Encryption key.
-
         if (isset($_COOKIE[$name][0]) && is_string($_COOKIE[$name])) {
             $value = $this->Rijndael256->decrypt($_COOKIE[$name], $key);
         }
