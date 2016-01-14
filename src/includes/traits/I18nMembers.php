12744722995ae2bb71cbda48cc1cb56227f5c13d
jaswsinc <jaswsinc@wsharks.com>
2016-01-13 18:00:56 -0900

    Bug fix. `__()` should public.

--- src/includes/traits/I18nMembers.php
+++ src/includes/traits/I18nMembers.php
@@ -23,7 +23,7 @@ trait I18nMembers
      *
      * @return string Translated string.
      */
-    protected function /* @codingStandardsIgnoreStart */ __ /*@codingStandardsIgnoreEnd*/(string $string): string
+    public function /* @codingStandardsIgnoreStart */ __ /*@codingStandardsIgnoreEnd*/(string $string): string
     {
         if ($this->App->Config->i18n['text_domain']) {
             return $string;
