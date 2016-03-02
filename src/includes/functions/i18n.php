92a0f4ec454feb9a58ebe3981119248c758a6910
jaswsinc <jaswsinc@wsharks.com>
2016-03-02 10:31:06 -0900

    Updating i18n utils.

--- src/includes/functions/i18n.php
+++ src/includes/functions/i18n.php
@@ -1,12 +1,41 @@
 <?php
 declare (strict_types = 1);
 
-if (!defined('ABSPATH') && !defined('WPINC') && !function_exists('__')) {
-    /**
-     * @since 160223 Polyfill.
-     */
-    function __(string $string): string
-    {
-        return $string;
+if (!defined('ABSPATH') && !defined('WPINC')) {
+    if (!function_exists('__')) {
+        /**
+         * @since 160223 Polyfill.
+         */
+        function __(string $string): string
+        {
+            return $string;
+        }
+    }
+    if (!function_exists('_x')) {
+        /**
+         * @since 160223 Polyfill.
+         */
+        function _x(string $string, string $context): string
+        {
+            return $string;
+        }
+    }
+    if (!function_exists('_n')) {
+        /**
+         * @since 160223 Polyfill.
+         */
+        function _n(string $singular, string $plural, float $count): string
+        {
+            return $count !== 1 && $count !== (float) 1 ? $plural : $singular;
+        }
+    }
+    if (!function_exists('_nx')) {
+        /**
+         * @since 160223 Polyfill.
+         */
+        function _nx(string $singular, string $plural, float $count, string $context): string
+        {
+            return $count !== 1 && $count !== (float) 1 ? $plural : $singular;
+        }
     }
 }
