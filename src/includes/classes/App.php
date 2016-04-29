4a5d686156e0ed5cc71d3ad297ff0b9698466a17
jaswsinc <jaswsinc@wsharks.com>
2016-04-28 20:12:46 -0800

    Updating build.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -232,7 +232,7 @@ class App extends Classes\Core\Base\Core
      *
      * @type string Version.
      */
-    const VERSION = '160427'; //v//
+    const VERSION = '160429'; //v//
 
     /**
      * Constructor.
