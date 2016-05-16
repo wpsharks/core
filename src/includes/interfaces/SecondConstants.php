4c3841eae0cde91efd683539efbfd188a9c11217
jaswsinc <jaswsinc@wsharks.com>
2016-05-15 23:27:19 -0800

    Adding `SecondConstants{}`

---
+++ src/includes/interfaces/SecondConstants.php
@@ -0,0 +1,56 @@
+<?php
+declare (strict_types = 1);
+namespace WebSharks\Core\Interfaces;
+
+/**
+ * Second constants.
+ *
+ * @since 160515 Second constants.
+ */
+interface SecondConstants
+{
+    /**
+     * Seconds in minute.
+     *
+     * @since 160515 Second constants.
+     *
+     * @type int Seconds in minute.
+     */
+    const SECONDS_IN_MINUTE = 60;
+
+    /**
+     * Seconds in hour.
+     *
+     * @since 160515 Second constants.
+     *
+     * @type int Seconds in hour.
+     */
+    const SECONDS_IN_HOUR = 3600;
+
+    /**
+     * Seconds in day.
+     *
+     * @since 160515 Second constants.
+     *
+     * @type int Seconds in day.
+     */
+    const SECONDS_IN_DAY = 86400;
+
+    /**
+     * Seconds in week.
+     *
+     * @since 160515 Second constants.
+     *
+     * @type int Seconds in week.
+     */
+    const SECONDS_IN_WEEK = 604800;
+
+    /**
+     * Seconds in year.
+     *
+     * @since 160515 Second constants.
+     *
+     * @type int Seconds in year.
+     */
+    const SECONDS_IN_YEAR = 31536000;
+}
