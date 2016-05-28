63f3e955663ea24e9612006950bd5da96b1db36c
jaswsinc <jaswsinc@wsharks.com>
2016-05-28 01:56:18 -0800

    Preparing release.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -235,7 +235,7 @@ class App extends Classes\Core\Base\Core
      *
      * @type string Version.
      */
-    const VERSION = '160528.35662'; //v//
+    const VERSION = '160528.35724'; //v//
 
     /**
      * Constructor.
