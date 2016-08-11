07a183ec7ecd86e01e1934282c15d209019551de
jaswsinc <jaswsinc@wsharks.com>
2016-08-11 13:11:49 -0800

    Adding Psysh stub.

---
+++ src/includes/psysh-stub.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Stub.
+ *
+ * @author @jaswsinc
+ * @copyright WebSharks™
+ */
+declare (strict_types = 1);
+namespace WebSharks\Core;
+
+use WebSharks\Core\Classes\App;
+
+require_once __DIR__.'/stub.php';
+
+ob_start(); // Use local config file as a base.
+include dirname(__FILE__, 3).'/assets/.~config.json';
+${__FILE__}['instance_base'] = json_decode(ob_get_clean(), true);
+
+$App = new App(${__FILE__}['instance_base'], [
+    '©debug' => [
+        '©enable'        => true,
+        '©log'           => true,
+        '©er_enable'     => true,
+        '©er_display'    => true,
+        '©er_assertions' => true,
+    ],
+    '©fs_paths' => [ // Use config. file.
+        '©logs_dir'  => dirname(__FILE__, 2).'/local/.~logs',
+        '©cache_dir' => dirname(__FILE__, 2).'/local/.~cache',
+    ],
+]);
+class_alias('WebSharks\\Core\\Classes\\CoreFacades', 'c');
