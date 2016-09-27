485b334c7989a106b70250ef4ea00f3e9ee2ab3c
jaswsinc <me@jaswsinc.com>
2016-09-27 12:23:18 -0800

    Adding sha1Mod()

--- src/client-s/js/core.js
+++ src/client-s/js/core.js
@@ -12,36 +12,43 @@
 
   _.mixin({
     crc32: function (str) {
-      return Hashes.CRC32(str);
+      return Hashes.CRC32(String(str));
     },
     md5: function (str) {
-      return md5.hex(str);
+      return md5.hex(String(str));
     },
     base64: function (str) {
-      return base64.hex(str);
+      return base64.hex(String(str));
     },
 
     sha1: function (str) {
-      return sha1.hex(str);
+      return sha1.hex(String(str));
     },
     sha256: function (str) {
-      return sha256.hex(str);
+      return sha256.hex(String(str));
     },
     sha512: function (str) {
-      return sha512.hex(str);
+      return sha512.hex(String(str));
     },
     rmd160: function (str) {
-      return rmd160.hex(str);
+      return rmd160.hex(String(str));
     },
 
     trim: function (str, chars) {
       return _.lTrim(_.rTrim(str, chars), chars);
     },
     lTrim: function (str, chars) {
-      return str.replace(new RegExp('^[' + (chars || '\\s') + ']+', 'g'), '');
+      return String(str).replace(new RegExp('^[' + (chars || '\\s') + ']+', 'g'), '');
     },
     rTrim: function (str, chars) {
-      return str.replace(new RegExp('[' + (chars || '\\s') + ']+$', 'g'), '');
+      return String(str).replace(new RegExp('[' + (chars || '\\s') + ']+$', 'g'), '');
+    },
+
+    hexDec: function (hex) {
+      return parseInt(String(str).replace(/[^a-f0-9]/i, ''), 16);
+    },
+    sha1Mod: function (str, divisor, isSha) {
+      return _.hexDec(String(isSha ? str : _.sha1(str)).substr(0, 15)) % Math.max(1, Number(divisor));
     }
   });
 
