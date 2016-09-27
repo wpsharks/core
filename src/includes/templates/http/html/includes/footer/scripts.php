23fd458e536daecc7b9f9dcecaf17f17debbba7a
jaswsinc <me@jaswsinc.com>
2016-09-27 08:53:37 -0800

    Adding jsHashes lib.

--- src/includes/templates/http/html/includes/footer/scripts.php
+++ src/includes/templates/http/html/includes/footer/scripts.php
@@ -20,6 +20,7 @@ use function get_defined_vars as vars;
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.7.0/highlight.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jshashes/1.0.5/hashes.min.js"></script>
 
 <script src="<?= $this->c::escUrl($this->c::appWsCoreUrl('/client-s/js/core.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
 <script src="<?= $this->c::escUrl($this->c::appWsCoreUrl('/client-s/js/hljs/langs/wp.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
