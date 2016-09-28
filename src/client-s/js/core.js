72f6c5b18ecaa4945122b8e99cb3a0aa165a2a08
jaswsinc <me@jaswsinc.com>
2016-09-28 09:07:22 -0800

    Reorganizing mixins.

--- src/client-s/js/core.js
+++ src/client-s/js/core.js
@@ -1,57 +1,4 @@
-(function ($) {
-  /*
-   * Mixins.
-   */
-  var crc32 = Hashes.CRC32;
-  var md5 = new Hashes.MD5();
-  var base64 = new Hashes.Base64();
-
-  var sha1 = new Hashes.SHA1();
-  var sha256 = new Hashes.SHA256();
-  var sha512 = new Hashes.SHA512();
-  var rmd160 = new Hashes.RMD160();
-
-  _.mixin({
-    crc32: function (str) {
-      return crc32(str);
-    },
-    md5: function (str) {
-      return md5.hex(str);
-    },
-    base64: function (str) {
-      return base64.hex(str);
-    },
-
-    sha1: function (str) {
-      return sha1.hex(str);
-    },
-    sha256: function (str) {
-      return sha256.hex(str);
-    },
-    sha512: function (str) {
-      return sha512.hex(str);
-    },
-    rmd160: function (str) {
-      return rmd160.hex(str);
-    },
-
-    trim: function (str, chars) {
-      return _.lTrim(_.rTrim(str, chars), chars);
-    },
-    lTrim: function (str, chars) {
-      return str.replace(new RegExp('^[' + (chars || '\\s') + ']+', 'g'), '');
-    },
-    rTrim: function (str, chars) {
-      return str.replace(new RegExp('[' + (chars || '\\s') + ']+$', 'g'), '');
-    },
-
-    sha1Mod: function (str, divisor, isSha) {
-      var sha1 = isSha ? str : _.sha1(str);
-      var hex15 = '0x' + sha1.substr(0, 15);
-      return new BigNumber(hex15).mod(Math.max(1, divisor)).toString(10);
-    }
-  });
-
+(function () {
   /*
    * On DOM ready handler.
    */
@@ -69,4 +16,4 @@
       }
     });
   });
-})(jQuery);
+})();
--- src/includes/templates/http/html/includes/footer/scripts.php
+++ src/includes/templates/http/html/includes/footer/scripts.php
@@ -26,8 +26,10 @@ use function get_defined_vars as vars;
 <script src="//cdnjs.cloudflare.com/ajax/libs/bignumber.js/2.4.0/bignumber.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jshashes/1.0.5/hashes.min.js"></script>
 
-<script src="<?= $this->c::escUrl($this->c::appWsCoreUrl('/client-s/js/core.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
+<script src="<?= $this->c::escUrl($this->c::appWsCoreUrl('/client-s/js/underscore/mixins.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
 <script src="<?= $this->c::escUrl($this->c::appWsCoreUrl('/client-s/js/hljs/langs/wp.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
+
+<script src="<?= $this->c::escUrl($this->c::appWsCoreUrl('/client-s/js/core.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
 
 <?php if (is_file($this->App->base_dir.'/src/client-s/js/app.min.js')) : ?>
     <script src="<?= $this->c::escUrl($this->c::appUrl('/client-s/js/app.min.js?v='.urlencode($body['scripts']['v']))); ?>"></script>
