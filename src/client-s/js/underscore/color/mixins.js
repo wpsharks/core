b15f42b721b553ab21ad71373952a2d9a00f1be5
jaswsinc <me@jaswsinc.com>
2016-10-12 19:44:52 -0800

    Bug fix in core mixin.

--- src/client-s/js/underscore/color/mixins.js
+++ src/client-s/js/underscore/color/mixins.js
@@ -6,7 +6,7 @@
   _.mixin({
 
     sha1Color: function (str, adjust) {
-      var hex = '#' + _.sha1(str).substring(0, 6);
+      var hex = '#' + _.sha1(str).substr(0, 6);
       return adjust ? _.adjustColorLuminosity(hex, adjust) : hex;
     },
 
@@ -16,9 +16,9 @@
         hex = hex.replace(/(.)/g, '$1$1');
       }
       for (var adjustedHex = '', _dec, _hex, _i = 0; _i < 3; _i++) {
-        _dec = parseInt(hex.substring(_i * 2, 2), 16);
+        _dec = parseInt(hex.substr(_i * 2, 2), 16);
         _hex = Math.round(Math.min(Math.max(0, _dec + (_dec * adjust)), 255)).toString(16);
-        adjustedHex += ('00' + _hex).substring(_hex.length);
+        adjustedHex += ('00' + _hex).substr(_hex.length);
       }
       return '#' + adjustedHex;
     }
