fc03073a73e65be1e519472d6e433d90e515c313
jaswrks <jas@src.works>
2017-03-19 22:58:25 -0800

    Updating build.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -183,7 +183,7 @@ class App extends Classes\Core\Base\Core
      *
      * @var string Version.
      */
-    const VERSION = '170319.44064'; //v//
+    const VERSION = '170320.24256'; //v//
 
     /**
      * Constructor.
