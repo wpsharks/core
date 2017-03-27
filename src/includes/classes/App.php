d5b4c1de3e4c4850011f1c6e11046aabc6cc3cd8
jaswrks <jas@src.works>
2017-03-27 05:36:22 -0800

    Enhancing alighment utils.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -183,7 +183,7 @@ class App extends Classes\Core\Base\Core
      *
      * @var string Version.
      */
-    const VERSION = '170327.48401'; //v//
+    const VERSION = '170327.48894'; //v//
 
     /**
      * Constructor.
