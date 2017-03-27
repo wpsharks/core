09c2fb381a40871bc939da01b21201803b437b70
jaswrks <jas@src.works>
2017-03-27 00:12:31 -0800

    Optimizing font events.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -183,7 +183,7 @@ class App extends Classes\Core\Base\Core
      *
      * @var string Version.
      */
-    const VERSION = '170327.28515'; //v//
+    const VERSION = '170327.29167'; //v//
 
     /**
      * Constructor.
