968d727784de451b1d6bf177f50eecbb5ce2f753
Jason Caldwell <jas@src.works>
2017-06-20 06:01:35 -0800

    Updating to latest PHP Markdown + CSS tweaks.

--- src/includes/classes/App.php
+++ src/includes/classes/App.php
@@ -183,7 +183,7 @@ class App extends Classes\Core\Base\Core
      *
      * @var string Version.
      */
-    const VERSION = '170620.44219'; //v//
+    const VERSION = '170620.50333'; //v//
 
     /**
      * Constructor.
