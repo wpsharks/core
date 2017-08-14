1f50257849af19ae0705d0886eec19cdad4c7331
Jason Caldwell <jas@src.works>
2017-08-13 18:56:25 -0800

    Enhancing Hash JS framework.

--- src/client-s/js/hash/default-app.js
+++ src/client-s/js/hash/default-app.js
@@ -4,34 +4,38 @@ var Hash;
     var App = (function () {
         // Constructor.
         function App() {
-            this.styleResourcesToLoad = [];
-            this.scriptResourcesToLoad = [];
-            this.stylesToLoadAsync = [];
-            this.scriptsToLoadAsync = [];
-            this.styleResourcesLoading = 0;
-            this.scriptResourcesLoading = 0;
-            this.styleResourcesReady = false;
-            this.scriptResourcesReady = false;
-            this.resourcesReady = false;
-            this.domContentLoaded = false;
-            this.scriptResourcesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-            this.scriptResourcesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js');
-            if (this.hasCode()) {
-                this.styleResourcesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/codepen-embed.min.css');
-                this.stylesToLoadAsync.push('https://cloud.typography.com/7715196/6490572/css/fonts.css');
-                this.scriptResourcesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js');
-                this.scriptResourcesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/typescript.min.js');
-                this.scriptResourcesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/scss.min.js');
-            }
-            this.styleResourcesLoading = this.styleResourcesToLoad.length;
-            this.scriptResourcesLoading = this.scriptResourcesToLoad.length;
-            document.addEventListener('DOMContentLoaded', this.onDomContentLoaded.bind(this));
-            this.loadStyles(), this.loadScripts();
+            var _this = this;
+            // Properties.
+            this.cache = {};
+            this.resourceUrls = {
+                $: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+                _: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js',
+                codeFonts: 'https://cloud.typography.com/7715196/6490572/css/fonts.css',
+                hljs: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js',
+                hljsScssLang: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/scss.min.js',
+                hljsTsLang: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/typescript.min.js',
+                hljsTheme: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/codepen-embed.min.css',
+            };
+            chopChop.preloadThenLoad(window.$ ? [] : [this.resourceUrls.$]).then(function () {
+                $(document).ready(_this.onDomjQueryReady.bind(_this));
+            });
         }
-        // Ready handler.
-        App.prototype.onDomResourcesReady = function () {
-            if (this.hasCode())
-                this.setupHljs();
+        // Ready handlers.
+        App.prototype.onDomjQueryReady = function () {
+            if (this.hasCode()) {
+                chopChop.preloadThenLoad([this.resourceUrls.codeFonts]);
+            }
+            if (this.hasPreCode()) {
+                chopChop.preloadThenLoad([
+                    this.resourceUrls.hljs,
+                    this.resourceUrls.hljsScssLang,
+                    this.resourceUrls.hljsTsLang,
+                    this.resourceUrls.hljsTheme,
+                ]).then(this.setupHljs.bind(this));
+            }
+            chopChop.preloadThenLoad(window._ ? [] : [this.resourceUrls._]).then(this.onReady.bind(this));
+        };
+        App.prototype.onReady = function () {
             if (typeof window.onReady === 'function') {
                 onReady(); // App ready handler.
             }
@@ -52,132 +56,16 @@ var Hash;
                 }
             });
         };
-        // Load utilities.
-        App.prototype.loadStyle = function (url) {
-            this.supportsPreload() ? this.preloadStyle(url) : this.loadStyleNow(url);
-        };
-        App.prototype.loadScript = function (url) {
-            this.supportsPreload() ? this.preloadScript(url) : this.loadScriptNow(url);
-        };
-        // Detection tools.
+        // Detection utilities.
         App.prototype.hasCode = function () {
-            if (this._hasCode !== undefined)
-                return this._hasCode;
-            return this._hasCode = document.querySelector('code') ? true : false;
+            if (typeof this.cache.hasCode !== undefined)
+                return this.cache.hasCode;
+            return this.cache.hasCode = $('code').length > 0;
         };
-        App.prototype.supportsPreload = function () {
-            if (this._supportsPreload !== undefined)
-                return this._supportsPreload;
-            var link = document.createElement('link');
-            return this._supportsPreload = link.relList && link.relList.supports && link.relList.supports('preload');
-        };
-        // Style loaders.
-        App.prototype.loadStyles = function () {
-            var _this = this;
-            if (this.styleResourcesToLoad.length > 0) {
-                this.styleResourcesToLoad.forEach(function (url) {
-                    _this.loadStyle(url);
-                });
-            }
-            else {
-                this.onStyleResourcesReady();
-            }
-        };
-        App.prototype.preloadStyle = function (url) {
-            var _this = this;
-            var l = document.createElement('link');
-            l.rel = 'preload', l.as = 'style', l.href = url, l.onload = function () { return _this.loadStyleNow(url); };
-            document.querySelector('head').appendChild(l);
-        };
-        App.prototype.loadStyleNow = function (url, async) {
-            var _this = this;
-            if (async === void 0) { async = false; }
-            var s = document.createElement('link');
-            s.href = url, s.rel = 'stylesheet', s.onload = function () { return _this.onStyleLoaded(url); };
-            document.querySelector('head').appendChild(s);
-        };
-        App.prototype.onStyleLoaded = function (url) {
-            if (this.styleIsResource(url)) {
-                this.styleResourcesLoading--;
-                if (this.styleResourcesLoading <= 0) {
-                    this.onStyleResourcesReady();
-                }
-            }
-        };
-        App.prototype.styleIsResource = function (url) {
-            return this.styleResourcesToLoad.indexOf(url) !== -1;
-        };
-        // Script load handlers.
-        App.prototype.loadScripts = function () {
-            var _this = this;
-            if (this.scriptResourcesToLoad.length > 0) {
-                this.scriptResourcesToLoad.forEach(function (url) {
-                    _this.loadScript(url);
-                });
-            }
-            else {
-                this.onScriptResourcesReady();
-            }
-        };
-        App.prototype.preloadScript = function (url) {
-            var _this = this;
-            var l = document.createElement('link');
-            l.rel = 'preload', l.as = 'script', l.href = url, l.onload = function () { return _this.loadScriptNow(url); };
-            document.querySelector('head').appendChild(l);
-        };
-        App.prototype.loadScriptNow = function (url, async) {
-            var _this = this;
-            if (async === void 0) { async = false; }
-            var s = document.createElement('script');
-            s.async = async, s.src = url, s.onload = function () { return _this.onScriptLoaded(url); };
-            document.querySelector('head').appendChild(s);
-        };
-        App.prototype.onScriptLoaded = function (url) {
-            if (this.scriptIsResource(url)) {
-                this.scriptResourcesLoading--;
-                if (this.scriptResourcesLoading <= 0) {
-                    this.onScriptResourcesReady();
-                }
-            }
-        };
-        App.prototype.scriptIsResource = function (url) {
-            return this.scriptResourcesToLoad.indexOf(url) !== -1;
-        };
-        // Resource ready handlers.
-        App.prototype.onStyleResourcesReady = function () {
-            var _this = this;
-            this.styleResourcesReady = true;
-            if (this.scriptResourcesReady)
-                this.onResourcesReady();
-            this.stylesToLoadAsync.forEach(function (url) {
-                _this.loadStyleNow(url, true);
-            });
-        };
-        App.prototype.onScriptResourcesReady = function () {
-            var _this = this;
-            this.scriptResourcesReady = true;
-            if (this.styleResourcesReady)
-                this.onResourcesReady();
-            this.scriptsToLoadAsync.forEach(function (url) {
-                _this.loadScriptNow(url, true);
-            });
-        };
-        // Other ready handlers.
-        App.prototype.onResourcesReady = function () {
-            if (this.resourcesReady)
-                return;
-            this.resourcesReady = true;
-            if (this.domContentLoaded) {
-                this.onDomResourcesReady();
-            }
-        };
-        App.prototype.onDomContentLoaded = function () {
-            if (this.domContentLoaded)
-                return;
-            this.domContentLoaded = true;
-            if (this.resourcesReady) {
-                this.onDomResourcesReady();
-            }
+        App.prototype.hasPreCode = function () {
+            if (typeof this.cache.hasPreCode !== undefined)
+                return this.cache.hasPreCode;
+            return this.cache.hasPreCode = $('pre > code').length > 0;
         };
         return App;
     }());
--- src/client-s/js/hash/default-app/default-app.ts
+++ src/client-s/js/hash/default-app/default-app.ts
@@ -2,48 +2,45 @@ namespace Hash {
   export class App {
     // Properties.
 
-    protected _hasCode?: boolean;
-    protected _supportsPreload?: boolean;
+    protected cache: { [ key: string ]: any } = {};
+    protected resourceUrls = {
+      $: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
+      _: 'https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js',
 
-    protected stylesToLoad: string[] = [];
-    protected scriptsToLoad: string[] = [];
+      codeFonts: 'https://cloud.typography.com/7715196/6490572/css/fonts.css',
 
-    protected stylesToLoadAsync: string[] = [];
-    protected scriptsToLoadAsync: string[] = [];
-
-    protected stylesLoading = 0;
-    protected scriptsLoading = 0;
-
-    protected styleResourcesReady = false;
-    protected scriptResourcesReady = false;
-
-    protected resourcesReady = false;
-    protected domContentLoaded = false;
+      hljs: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js',
+      hljsScssLang: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/scss.min.js',
+      hljsTsLang: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/typescript.min.js',
+      hljsTheme: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/codepen-embed.min.css',
+    };
 
     // Constructor.
 
     public constructor() {
-      this.scriptsToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js');
-      this.scriptsToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js');
-
-      if (this.hasCode()) {
-        this.stylesToLoadAsync.push('https://cloud.typography.com/7715196/6490572/css/fonts.css');
-        this.stylesToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/codepen-embed.min.css');
-
-        this.scriptsToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js');
-        this.scriptsToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/scss.min.js');
-        this.scriptsToLoad.push('https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/languages/typescript.min.js');
-      }
-      this.stylesLoading = this.stylesToLoad.length, this.scriptsLoading = this.scriptsToLoad.length;
-      document.addEventListener('DOMContentLoaded', this.onDomContentLoaded.bind(this));
-      this.loadStyles(), this.loadScripts();
+      chopChop.preloadThenLoad(window.$ ? [] : [ this.resourceUrls.$ ]).then(() => {
+        $(document).ready(this.onDomjQueryReady.bind(this));
+      });
     }
 
-    // Ready handler.
+    // Ready handlers.
 
-    protected onDomResourcesReady() {
-      if (this.hasCode()) this.setupHljs();
+    protected onDomjQueryReady() {
+      if (this.hasCode()) {
+        chopChop.preloadThenLoad([ this.resourceUrls.codeFonts ]);
+      }
+      if (this.hasPreCode()) {
+        chopChop.preloadThenLoad([
+          this.resourceUrls.hljs,
+          this.resourceUrls.hljsScssLang,
+          this.resourceUrls.hljsTsLang,
+          this.resourceUrls.hljsTheme,
+        ]).then(this.setupHljs.bind(this));
+      }
+      chopChop.preloadThenLoad(window._ ? [] : [ this.resourceUrls._ ]).then(this.onReady.bind(this));
+    }
 
+    protected onReady() {
       if (typeof window.onReady === 'function') {
         onReady(); // App ready handler.
       }
@@ -69,158 +66,18 @@ namespace Hash {
       });
     }
 
-    // Load utilities.
+    // Detection utilities.
 
-    public loadStyle(url: string) {
-      this.supportsPreload() ? this.preloadStyle(url) : this.loadStyleNow(url);
+    protected hasCode(): boolean {
+      if (typeof this.cache.hasCode !== undefined)
+        return this.cache.hasCode;
+      return this.cache.hasCode = $('code').length > 0;
     }
 
-    public loadScript(url: string) {
-      this.supportsPreload() ? this.preloadScript(url) : this.loadScriptNow(url);
-    }
-
-    // Detection tools.
-
-    public hasCode(): boolean {
-      if (this._hasCode !== undefined)
-        return this._hasCode;
-
-      return this._hasCode = document.querySelector('code') ? true : false;
-    }
-
-    public supportsPreload(): boolean {
-      if (this._supportsPreload !== undefined)
-        return <boolean>this._supportsPreload;
-
-      let link = <HTMLElement | any>document.createElement('link');
-      return this._supportsPreload = link.relList && link.relList.supports && link.relList.supports('preload');
-    }
-
-    // Style loaders.
-
-    protected loadStyles() {
-      if (this.stylesToLoad.length > 0) {
-        this.stylesToLoad.forEach((url) => {
-          this.loadStyle(url);
-        });
-      } else {
-        this.onStyleResourcesReady();
-      }
-    }
-
-    protected preloadStyle(url: string) {
-      let l = <HTMLElement | any>document.createElement('link');
-      l.rel = 'preload', l.as = 'style', l.href = url, l.onload = () => this.loadStyleNow(url);
-      (<HTMLElement>document.querySelector('head')).appendChild(l);
-    }
-
-    protected loadStyleNow(url: string, async: boolean = false) {
-      let s = document.createElement('link');
-      s.href = url, s.rel = 'stylesheet', s.onload = () => this.onStyleLoaded(url);
-      (<HTMLElement>document.querySelector('head')).appendChild(s);
-    }
-
-    protected onStyleLoaded(url: string) {
-      if (this.styleIsResource(url)) {
-        this.stylesLoading--;
-        if (this.stylesLoading <= 0) {
-          this.onStyleResourcesReady();
-        }
-      }
-    }
-
-    protected styleIsResource(url: string) {
-      return this.stylesToLoad.indexOf(url) !== -1;
-    }
-
-    // Script load handlers.
-
-    protected loadScripts() {
-      if (this.scriptsToLoad.length > 0) {
-        this.scriptsToLoad.forEach((url) => {
-          this.loadScript(url);
-        });
-      } else {
-        this.onScriptResourcesReady();
-      }
-    }
-
-    protected preloadScript(url: string) {
-      let l = <HTMLElement | any>document.createElement('link');
-      l.rel = 'preload', l.as = 'script', l.href = url, l.onload = () => this.loadScriptNow(url);
-      (<HTMLElement>document.querySelector('head')).appendChild(l);
-    }
-
-    protected loadScriptNow(url: string, async: boolean = false) {
-      let s = document.createElement('script');
-      s.async = async, s.src = url, s.onload = () => this.onScriptLoaded(url);
-      (<HTMLElement>document.querySelector('head')).appendChild(s);
-    }
-
-    protected onScriptPreloaded(url: string) {
-      if (this.scriptIsResource(url)) {
-        this.scriptsLoading--;
-        if (this.scriptsLoading <= 0) {
-          this.onScriptResourcesReady();
-        }
-      }
-    }
-
-    protected onScriptLoaded(url: string) {
-      if (this.scriptIsResource(url)) {
-        this.scriptsLoading--;
-        if (this.scriptsLoading <= 0) {
-          this.onScriptResourcesReady();
-        }
-      }
-    }
-
-    protected scriptIsResource(url: string) {
-      return this.scriptsToLoad.indexOf(url) !== -1;
-    }
-
-    // Resource ready handlers.
-
-    protected onStyleResourcesReady() {
-      this.styleResourcesReady = true;
-
-      if (this.scriptResourcesReady)
-        this.onResourcesReady();
-
-      this.stylesToLoadAsync.forEach((url) => {
-        this.loadStyleNow(url, true);
-      });
-    }
-
-    protected onScriptResourcesReady() {
-      this.scriptResourcesReady = true;
-
-      if (this.styleResourcesReady)
-        this.onResourcesReady();
-
-      this.scriptsToLoadAsync.forEach((url) => {
-        this.loadScriptNow(url, true);
-      });
-    }
-
-    // Other ready handlers.
-
-    protected onResourcesReady() {
-      if (this.resourcesReady) return;
-      this.resourcesReady = true;
-
-      if (this.domContentLoaded) {
-        this.onDomResourcesReady();
-      }
-    }
-
-    protected onDomContentLoaded() {
-      if (this.domContentLoaded) return;
-      this.domContentLoaded = true;
-
-      if (this.resourcesReady) {
-        this.onDomResourcesReady();
-      }
+    protected hasPreCode(): boolean {
+      if (typeof this.cache.hasPreCode !== undefined)
+        return this.cache.hasPreCode;
+      return this.cache.hasPreCode = $('pre > code').length > 0;
     }
   }
   window.app = new App();
